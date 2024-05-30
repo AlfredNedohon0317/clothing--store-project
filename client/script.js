150ff074c8daaad337b25dfa39593bd295fbd819
jslempa <jslempa@gmail.com>
2024-05-30 11:06:50 -0400

    connected brand data to front end

--- client/script.js
+++ client/script.js
@@ -1,7 +1,13 @@
+const bodyEl = document.querySelector('body')
+const headingEl = document.querySelector('h1')
+
 async function getInfo() {
     const res = await axios.get(`http://localhost:3001/brands`);
 
-    console.log(res.data)
+    const brandData = res.data
+
+    // console.log(res.data)
+    console.log(brandData[0].name)
 
     
 }
