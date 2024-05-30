64435f6859a070a8dff0ebb5dff17e64827b0c47
jslempa <jslempa@gmail.com>
2024-05-30 11:45:21 -0400

    added brand img elements

--- client/script.js
+++ client/script.js
@@ -8,16 +8,28 @@ async function getInfo() {
 
     brandData.forEach((brand) => {
         console.log(brand.name)
-        const brandEl = document.createElement('h2')
-        brandEl.innerHTML = brand.name
+        const brandEl = document.createElement('img')
+        //brandEl.innerHTML = brand.name
+        if (brand.name === 'Chanel') {
+            brandEl.src = ''//chanel pic
+        } else if (brand.name === 'Dior') {
+            brandEl.src = ''//dior pic
+        } else if (brand.name === 'Gucci') {
+            brandEl.src = ''//gucci pic
+        } else if (brand.name === 'Louis Vuitton') {
+            brandEl.src = ''//lv pic
+        } else if (brand.name === 'Versace') {
+            brandEl.src = ''//versace pic
+        } else {
+            console.log('Image not available')
+        }
         bodyEl.appendChild(brandEl)
     })
-
    // console.log(brandData[0].name)
-
+}
 
 
     
-}
+
 
 getInfo()
