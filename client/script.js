c730eba7a1bbcc6442c93323858317ced54f7306
jslempa <jslempa@gmail.com>
2024-05-30 14:28:41 -0400

    added classes to images

--- client/script.js
+++ client/script.js
@@ -10,21 +10,21 @@ async function getInfo() {
         console.log(brand.name)
         const titleEl = document.createElement('h2')
         const brandEl = document.createElement('img')
-       
         brandEl.addEventListener('click', handleClick)
         if (brand.name === 'Chanel') {
+            brandEl.classList.add('Chanel')
             brandEl.src = 'https://i.pinimg.com/736x/32/43/f9/3243f9409fae41757d06fe6bc2d2d06d.jpg'//chanel pic
-
-
-
-
         } else if (brand.name === 'Dior') {
+            brandEl.classList.add('Dior')
             brandEl.src = 'https://images-prod.dazeddigital.com/540/azure/dazed-prod/1120/0/1120547.jpg'//dior pic
         } else if (brand.name === 'Gucci') {
+            brandEl.classList.add('Gucci')
             brandEl.src = 'https://theimpression.com/wp-content/uploads/2023/06/gucci-valigeria-2023-ad-campaign-the-impression-003.jpg'//gucci pic
         } else if (brand.name === 'Louis Vuitton') {
+            brandEl.classList.add('LouisVuitton')
             brandEl.src = 'https://www.rebag.com/thevault/wp-content/uploads/2023/06/rihanna-lv-mens-campaign-3-649999cb25888.jpeg'//lv pic
         } else if (brand.name === 'Versace') {
+            brandEl.classList.add('Versace')
             brandEl.src = 'https://hips.hearstapps.com/digitalspyuk.cdnds.net/13/47/gaga-versace.jpg?resize=768:*'//versace pic
         } else {
             console.log('Image not available')
@@ -35,7 +35,9 @@ async function getInfo() {
     })
 }
 
-
+const handleClick = (event) => {
+    console.log(event.target.classList)
+}
     
 
 
