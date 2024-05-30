ca3eea3f4615eeb98389ac7a1c7a7aaee7ea4c53
jslempa <jslempa@gmail.com>
2024-05-30 09:07:53 -0400

    connected to front end

--- server.js
+++ server.js
@@ -1,22 +1,25 @@
-
-const PORT = process.env.PORT || 3001;
 const express = require('express')
-const app = express()
 const db = require('./db')
+const cors = require('cors')
 const logger = require('morgan')
 const bodyParser = require('body-parser')
 const brandController = require('./controllers/brandController.js')
 const shirtController = require('./controllers/shirtController.js' )
 const pantController = require('./controllers/pantController.js' )
 
+db.on('error', console.error.bind(console, 'MongoDB connection error:'))
 
+const PORT = process.env.PORT || 3001
+
+const app = express()
+
+app.use(cors())
 app.use(logger('dev'))
 app.use(bodyParser.json())
 
 app.listen(PORT, () => console.log(`Listening on port: ${PORT}`))
 
-
-app.get('/', (req, res) => res.send('initilizing server back end!'))
+app.get('/', (req, res) => res.send('initializing server back end!'))
 
 app.get('/brands', brandController.getAllBrands)
 
