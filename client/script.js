b71082b705a559af910e114c089299ede71d7f1e
jslempa <jslempa@gmail.com>
2024-05-30 11:53:39 -0400

    added images

--- client/script.js
+++ client/script.js
@@ -11,15 +11,15 @@ async function getInfo() {
         const brandEl = document.createElement('img')
         //brandEl.innerHTML = brand.name
         if (brand.name === 'Chanel') {
-            brandEl.src = ''//chanel pic
+            brandEl.src = 'https://i.pinimg.com/736x/32/43/f9/3243f9409fae41757d06fe6bc2d2d06d.jpg'//chanel pic
         } else if (brand.name === 'Dior') {
-            brandEl.src = ''//dior pic
+            brandEl.src = 'https://images-prod.dazeddigital.com/540/azure/dazed-prod/1120/0/1120547.jpg'//dior pic
         } else if (brand.name === 'Gucci') {
-            brandEl.src = ''//gucci pic
+            brandEl.src = 'https://theimpression.com/wp-content/uploads/2023/06/gucci-valigeria-2023-ad-campaign-the-impression-003.jpg'//gucci pic
         } else if (brand.name === 'Louis Vuitton') {
-            brandEl.src = ''//lv pic
+            brandEl.src = 'https://www.rebag.com/thevault/wp-content/uploads/2023/06/rihanna-lv-mens-campaign-3-649999cb25888.jpeg'//lv pic
         } else if (brand.name === 'Versace') {
-            brandEl.src = ''//versace pic
+            brandEl.src = 'https://hips.hearstapps.com/digitalspyuk.cdnds.net/13/47/gaga-versace.jpg?resize=768:*'//versace pic
         } else {
             console.log('Image not available')
         }
